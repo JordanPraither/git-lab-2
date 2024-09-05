a85ef4b55a205bc7cf79654474bfc900ca3973c0
Jordan Praither <jp855622@ohio.edu>
2024-09-05 16:55:50 -0400

    New Program

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Jordan Praither
+ *        Date: 9/05/2024
+ * Description: CS 2400 lab 2
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
